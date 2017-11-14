84f1016731af03bb0e126db9c38c744c05b51e38
geeksploit <geeksploit@shootan.com>
2017-11-14 12:43:52 +0300

    Create the forecast menu resource file

--- S02.02-Exercise-Menus/app/src/main/java/com/example/android/sunshine/MainActivity.java
+++ S02.02-Exercise-Menus/app/src/main/java/com/example/android/sunshine/MainActivity.java
@@ -97,7 +97,7 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    // TODO (2) Create a menu resource in res/menu/ called forecast.xml
+    // COMPLETED (2) Create a menu resource in res/menu/ called forecast.xml
     // TODO (3) Add one item to the menu with an ID of action_refresh
     // TODO (4) Set the title of the menu item to "Refresh" using strings.xml
 
