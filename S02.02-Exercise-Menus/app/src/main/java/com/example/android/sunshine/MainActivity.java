8683c1a0d412f4f06a6a42fc42f9ac585a6c84e9
geeksploit <geeksploit@shootan.com>
2017-11-14 13:52:49 +0300

    Override onCreateOptionsMenu to inflate and display the menu

--- S02.02-Exercise-Menus/app/src/main/java/com/example/android/sunshine/MainActivity.java
+++ S02.02-Exercise-Menus/app/src/main/java/com/example/android/sunshine/MainActivity.java
@@ -18,6 +18,7 @@ package com.example.android.sunshine;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
 import android.widget.TextView;
 
 import com.example.android.sunshine.data.SunshinePreferences;
@@ -101,8 +102,13 @@ public class MainActivity extends AppCompatActivity {
     // COMPLETED (3) Add one item to the menu with an ID of action_refresh
     // COMPLETED (4) Set the title of the menu item to "Refresh" using strings.xml
 
-    // TODO (5) Override onCreateOptionsMenu to inflate the menu for this Activity
-    // TODO (6) Return true to display the menu
+    // COMPLETED (5) Override onCreateOptionsMenu to inflate the menu for this Activity
+    // COMPLETED (6) Return true to display the menu
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.forecast, menu);
+        return true;
+    }
 
     // TODO (7) Override onOptionsItemSelected to handle clicks on the refresh button
 }
