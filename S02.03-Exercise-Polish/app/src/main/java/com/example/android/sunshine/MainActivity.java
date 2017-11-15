63964cf23c731d9e6c76eaca1ba9a0f90244dbf6
geeksploit <geeksploit@shootan.com>
2017-11-15 14:44:22 +0300

    Show the data view if the weather data was not null

--- S02.03-Exercise-Polish/app/src/main/java/com/example/android/sunshine/MainActivity.java
+++ S02.03-Exercise-Polish/app/src/main/java/com/example/android/sunshine/MainActivity.java
@@ -116,7 +116,8 @@ public class MainActivity extends AppCompatActivity {
             // TODO (19) As soon as the data is finished loading, hide the loading indicator
 
             if (weatherData != null) {
-                // TODO (11) If the weather data was not null, make sure the data view is visible
+                // COMPLETED (11) If the weather data was not null, make sure the data view is visible
+                showWeatherDataView();
                 /*
                  * Iterate through the array and append the Strings to the TextView. The reason why we add
                  * the "\n\n\n" after the String is to give visual separation between each String in the
