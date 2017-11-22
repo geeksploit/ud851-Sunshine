dcff30a2c8f9d3d9cd48eb2cab482add190ec6b7
geeksploit <geeksploit@shootan.com>
2017-11-22 12:45:47 +0300

    Create a constructor and accept a View as a parameter

--- S03.01-Exercise-RecyclerView/app/src/main/java/com/example/android/sunshine/ForecastAdapter.java
+++ S03.01-Exercise-RecyclerView/app/src/main/java/com/example/android/sunshine/ForecastAdapter.java
@@ -1,10 +1,14 @@
 package com.example.android.sunshine;
 
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
 import android.widget.TextView;
 
 public class ForecastAdapter {
     class ForecastAdapterViewHolder extends RecyclerView.ViewHolder {
         public final TextView mWeatherTextView;
+
+        public ForecastAdapterViewHolder(View itemView) {
+        }
     }
 }
--- S03.01-Exercise-RecyclerView/app/src/main/java/com/example/android/sunshine/MainActivity.java
+++ S03.01-Exercise-RecyclerView/app/src/main/java/com/example/android/sunshine/MainActivity.java
@@ -63,7 +63,7 @@ public class MainActivity extends AppCompatActivity {
     // Within ForecastAdapterViewHolder ///////////////////////////////////////////////////////////
     // COMPLETED (18) Create a public final TextView variable called mWeatherTextView
 
-    // TODO (19) Create a constructor for this class that accepts a View as a parameter
+    // COMPLETED (19) Create a constructor for this class that accepts a View as a parameter
     // TODO (20) Call super(view) within the constructor for ForecastAdapterViewHolder
     // TODO (21) Using view.findViewById, get a reference to this layout's TextView and save it to mWeatherTextView
     // Within ForecastAdapterViewHolder ///////////////////////////////////////////////////////////
