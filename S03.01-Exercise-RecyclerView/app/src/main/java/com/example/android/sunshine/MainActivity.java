dbf4f3518fa3e267b75a1d7b3d08d3e982803919
geeksploit <geeksploit@shootan.com>
2017-11-22 12:32:51 +0300

    Add a View to the layout with a width of mathc_parent and height of 1dp

--- S03.01-Exercise-RecyclerView/app/src/main/java/com/example/android/sunshine/MainActivity.java
+++ S03.01-Exercise-RecyclerView/app/src/main/java/com/example/android/sunshine/MainActivity.java
@@ -43,7 +43,7 @@ public class MainActivity extends AppCompatActivity {
     // COMPLETED (10) Make the width and height wrap_content
     // COMPLETED (11) Give the TextView 16dp of padding
 
-    // TODO (12) Add a View to the layout with a width of match_parent and a height of 1dp
+    // COMPLETED (12) Add a View to the layout with a width of match_parent and a height of 1dp
     // TODO (13) Set the background color to #dadada
     // TODO (14) Set the left and right margins to 8dp
     // Within forecast_list_item.xml //////////////////////////////////////////////////////////////
