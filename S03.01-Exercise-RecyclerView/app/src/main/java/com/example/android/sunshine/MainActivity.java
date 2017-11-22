2c23d9a1f482f298bc457944b071ef0235a876a3
geeksploit <geeksploit@shootan.com>
2017-11-22 12:33:46 +0300

    Set the background color to #dadada

--- S03.01-Exercise-RecyclerView/app/src/main/java/com/example/android/sunshine/MainActivity.java
+++ S03.01-Exercise-RecyclerView/app/src/main/java/com/example/android/sunshine/MainActivity.java
@@ -44,7 +44,7 @@ public class MainActivity extends AppCompatActivity {
     // COMPLETED (11) Give the TextView 16dp of padding
 
     // COMPLETED (12) Add a View to the layout with a width of match_parent and a height of 1dp
-    // TODO (13) Set the background color to #dadada
+    // COMPLETED (13) Set the background color to #dadada
     // TODO (14) Set the left and right margins to 8dp
     // Within forecast_list_item.xml //////////////////////////////////////////////////////////////
 
