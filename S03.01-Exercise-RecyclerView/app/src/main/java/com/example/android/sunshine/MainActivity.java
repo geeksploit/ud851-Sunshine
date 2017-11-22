0ab7038bd42da1f252247aa2b15e16a89e5eeebd
geeksploit <geeksploit@shootan.com>
2017-11-22 15:57:29 +0300

    Get a reference to the RecyclerView

--- S03.01-Exercise-RecyclerView/app/src/main/java/com/example/android/sunshine/MainActivity.java
+++ S03.01-Exercise-RecyclerView/app/src/main/java/com/example/android/sunshine/MainActivity.java
@@ -103,7 +103,8 @@ public class MainActivity extends AppCompatActivity {
 
         // COMPLETED (36) Delete the line where you get a reference to mWeatherTextView
 
-        // TODO (37) Use findViewById to get a reference to the RecyclerView
+        // COMPETED (37) Use findViewById to get a reference to the RecyclerView
+        mRecyclerView = (RecyclerView) findViewById(R.id.recyclerview_forecast);
 
         /* This TextView is used to display errors and will be hidden if there are no errors */
         mErrorMessageDisplay = (TextView) findViewById(R.id.tv_error_message_display);
