cd64cc35e97e55a4dfc5005b8c55fb4e38dea246
geeksploit <geeksploit@shootan.com>
2017-11-23 13:03:41 +0300

    Show a Toast with weather data when an item is clicked

--- S03.02-Exercise-RecyclerViewClickHandling/app/src/main/java/com/example/android/sunshine/MainActivity.java
+++ S03.02-Exercise-RecyclerViewClickHandling/app/src/main/java/com/example/android/sunshine/MainActivity.java
@@ -26,6 +26,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.android.sunshine.data.SunshinePreferences;
 import com.example.android.sunshine.utilities.NetworkUtils;
@@ -108,9 +109,10 @@ public class MainActivity extends AppCompatActivity implements ForecastAdapter.F
     }
 
     // COMPLETED (9) Override ForecastAdapterOnClickHandler's onClick method
-    // TODO (10) Show a Toast when an item is clicked, displaying that item's weather data
+    // COMPLETED (10) Show a Toast when an item is clicked, displaying that item's weather data
     @Override
     public void onClick(String listItemData) {
+        Toast.makeText(this, listItemData, Toast.LENGTH_LONG).show();
     }
 
     /**
