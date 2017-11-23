e5b1c9fe4245076b1d4934522f0a0b7d9425f6a7
geeksploit <geeksploit@shootan.com>
2017-11-23 12:58:01 +0300

    Override onClick, passing the clicked day's weather data to click handler

--- S03.02-Exercise-RecyclerViewClickHandling/app/src/main/java/com/example/android/sunshine/ForecastAdapter.java
+++ S03.02-Exercise-RecyclerViewClickHandling/app/src/main/java/com/example/android/sunshine/ForecastAdapter.java
@@ -57,7 +57,12 @@ public class ForecastAdapter extends RecyclerView.Adapter<ForecastAdapter.Foreca
             // TODO (7) Call setOnClickListener on the view passed into the constructor (use 'this' as the OnClickListener)
         }
 
-        // TODO (6) Override onClick, passing the clicked day's data to mClickHandler via its onClick method
+        // COMPLETED (6) Override onClick, passing the clicked day's data to mClickHandler via its onClick method
+        @Override
+        public void onClick(View v) {
+            String weatherData = mWeatherData[getAdapterPosition()];
+            mClickHandler.onClick(weatherData);
+        }
     }
 
     /**
