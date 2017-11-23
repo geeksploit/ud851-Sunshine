86693bae97115585be1df039c52744cd8c66e2ba
geeksploit <geeksploit@shootan.com>
2017-11-23 12:54:50 +0300

    Implement OnClickListener in the view holder

--- S03.02-Exercise-RecyclerViewClickHandling/app/src/main/java/com/example/android/sunshine/ForecastAdapter.java
+++ S03.02-Exercise-RecyclerViewClickHandling/app/src/main/java/com/example/android/sunshine/ForecastAdapter.java
@@ -44,11 +44,11 @@ public class ForecastAdapter extends RecyclerView.Adapter<ForecastAdapter.Foreca
         mClickHandler = clickHandler;
     }
 
-    // TODO (5) Implement OnClickListener in the ForecastAdapterViewHolder class
+    // COMPLETED (5) Implement OnClickListener in the ForecastAdapterViewHolder class
     /**
      * Cache of the children views for a forecast list item.
      */
-    public class ForecastAdapterViewHolder extends RecyclerView.ViewHolder {
+    public class ForecastAdapterViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
         public final TextView mWeatherTextView;
 
         public ForecastAdapterViewHolder(View view) {
