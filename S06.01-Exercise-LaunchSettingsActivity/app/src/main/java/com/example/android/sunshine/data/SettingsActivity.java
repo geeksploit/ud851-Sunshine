75dcace0018b6264b7007af192e477c835ba6430
geeksploit <geeksploit@shootan.com>
2017-12-19 22:02:42 +0300

    Navigate back if the up button is pressed in ActionBar

--- S06.01-Exercise-LaunchSettingsActivity/app/src/main/java/com/example/android/sunshine/data/SettingsActivity.java
+++ S06.01-Exercise-LaunchSettingsActivity/app/src/main/java/com/example/android/sunshine/data/SettingsActivity.java
@@ -3,6 +3,7 @@ package com.example.android.sunshine.data;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 
 import com.example.android.sunshine.R;
 
@@ -17,5 +18,15 @@ public class SettingsActivity extends AppCompatActivity {
         if (actionBar != null) {
             actionBar.setDisplayHomeAsUpEnabled(true);
         }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == android.R.id.home) {
+            onBackPressed();
+
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
