d74b4a9e0a3c7be279bf8ba18d2bff7a9009df62
geeksploit <geeksploit@shootan.com>
2017-12-20 08:28:22 +0300

    Create SettingsFragment and extend PreferenceFragmentCompat

---
+++ S06.02-Exercise-SettingsFragment/app/src/main/java/com/example/android/sunshine/SettingsFragment.java
@@ -0,0 +1,10 @@
+package com.example.android.sunshine;
+
+import android.support.v7.preference.PreferenceFragmentCompat;
+
+/**
+ * Created by geeksploit on 19.12.2017.
+ */
+
+public class SettingsFragment extends PreferenceFragmentCompat {
+}
