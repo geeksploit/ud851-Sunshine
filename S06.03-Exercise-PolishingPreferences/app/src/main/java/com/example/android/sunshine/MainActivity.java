7e5a476cb5401d1ac7851f37db00b812892b419f
geeksploit <geeksploit@shootan.com>
2017-12-20 11:03:39 +0300

    Refresh the data by restarting the loader if preferences have been changed

--- S06.03-Exercise-PolishingPreferences/app/src/main/java/com/example/android/sunshine/MainActivity.java
+++ S06.03-Exercise-PolishingPreferences/app/src/main/java/com/example/android/sunshine/MainActivity.java
@@ -333,7 +333,15 @@ public class MainActivity extends AppCompatActivity implements
         mErrorMessageDisplay.setVisibility(View.VISIBLE);
     }
 
-    // TODO (7) In onStart, if preferences have been changed, refresh the data and set the flag to false
+    // COMPLETED (7) In onStart, if preferences have been changed, refresh the data and set the flag to false
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if (PREFERENCES_HAVE_BEEN_UPDATED) {
+            getSupportLoaderManager().restartLoader(FORECAST_LOADER_ID, null, this);
+            PREFERENCES_HAVE_BEEN_UPDATED = false;
+        }
+    }
 
     // TODO (8) Override onDestroy and unregister MainActivity as a SharedPreferenceChangedListener
 
