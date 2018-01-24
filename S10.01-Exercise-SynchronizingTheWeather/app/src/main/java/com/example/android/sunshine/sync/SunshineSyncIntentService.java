04f9486fd4e1fa1e9da23945a7d7d14d6175e5da
geeksploit <geeksploit@shootan.com>
2018-01-24 21:25:57 +0300

    Create a new class to handle weather synchronization in background

--- S10.01-Exercise-SynchronizingTheWeather/app/src/main/java/com/example/android/sunshine/sync/SunshineSyncIntentService.java
+++ S10.01-Exercise-SynchronizingTheWeather/app/src/main/java/com/example/android/sunshine/sync/SunshineSyncIntentService.java
@@ -1,3 +1,9 @@
-// TODO (5) Create a new class called SunshineSyncIntentService that extends IntentService
+package com.example.android.sunshine.sync;
+
+import android.app.IntentService;
+
+// COMPLETED (5) Create a new class called SunshineSyncIntentService that extends IntentService
+class SunshineSyncIntentService extends IntentService {
 //  TODO (6) Create a constructor that calls super and passes the name of this class
 //  TODO (7) Override onHandleIntent, and within it, call SunshineSyncTask.syncWeather
+}
