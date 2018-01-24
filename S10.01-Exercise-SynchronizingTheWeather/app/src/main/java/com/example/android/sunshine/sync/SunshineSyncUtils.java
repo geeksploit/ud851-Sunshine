c322dfe544498b18a97a568ee88d0b01e1609e8e
geeksploit <geeksploit@shootan.com>
2018-01-24 21:47:13 +0300

    Create a class called SunshineSyncUtils

--- S10.01-Exercise-SynchronizingTheWeather/app/src/main/java/com/example/android/sunshine/sync/SunshineSyncUtils.java
+++ S10.01-Exercise-SynchronizingTheWeather/app/src/main/java/com/example/android/sunshine/sync/SunshineSyncUtils.java
@@ -1,3 +1,7 @@
-// TODO (9) Create a class called SunshineSyncUtils
+package com.example.android.sunshine.sync;
+
+// COMPLETED (9) Create a class called SunshineSyncUtils
+class SunshineSyncUtils {
     //  TODO (10) Create a public static void method called startImmediateSync
     //  TODO (11) Within that method, start the SunshineSyncIntentService
+}
