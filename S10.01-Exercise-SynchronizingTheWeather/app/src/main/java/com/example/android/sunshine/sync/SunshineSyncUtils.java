62c0fdeb5432d2c1b0bba15ba325ace6b68caff8
geeksploit <geeksploit@shootan.com>
2018-01-24 21:49:21 +0300

    Create a method that starts synchronization at once

--- S10.01-Exercise-SynchronizingTheWeather/app/src/main/java/com/example/android/sunshine/sync/SunshineSyncUtils.java
+++ S10.01-Exercise-SynchronizingTheWeather/app/src/main/java/com/example/android/sunshine/sync/SunshineSyncUtils.java
@@ -1,7 +1,11 @@
 package com.example.android.sunshine.sync;
 
+import android.content.Context;
+
 // COMPLETED (9) Create a class called SunshineSyncUtils
 class SunshineSyncUtils {
-    //  TODO (10) Create a public static void method called startImmediateSync
+    // COMPLETED (10) Create a public static void method called startImmediateSync
+    public static void startImmediateSync(Context context) {
     //  TODO (11) Within that method, start the SunshineSyncIntentService
+    }
 }
