1b6bf6e88b5bd2771375912e319aeca9697d0bd6
geeksploit <geeksploit@shootan.com>
2018-01-24 21:55:00 +0300

    Start the synchronization service

--- S10.01-Exercise-SynchronizingTheWeather/app/src/main/java/com/example/android/sunshine/sync/SunshineSyncUtils.java
+++ S10.01-Exercise-SynchronizingTheWeather/app/src/main/java/com/example/android/sunshine/sync/SunshineSyncUtils.java
@@ -1,11 +1,14 @@
 package com.example.android.sunshine.sync;
 
 import android.content.Context;
+import android.content.Intent;
 
 // COMPLETED (9) Create a class called SunshineSyncUtils
 class SunshineSyncUtils {
     // COMPLETED (10) Create a public static void method called startImmediateSync
     public static void startImmediateSync(Context context) {
-    //  TODO (11) Within that method, start the SunshineSyncIntentService
+        // COMPLETED (11) Within that method, start the SunshineSyncIntentService
+        Intent synchronizationIntent = new Intent(context, SunshineSyncIntentService.class);
+        context.startService(synchronizationIntent);
     }
 }
