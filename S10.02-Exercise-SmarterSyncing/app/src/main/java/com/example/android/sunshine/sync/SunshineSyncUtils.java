67b7ae767151414b9a4c502cb3109dd66ef534dc
geeksploit <geeksploit@shootan.com>
2018-01-25 16:45:15 +0300

    Create a method that handles initialization depending on current status

--- S10.02-Exercise-SmarterSyncing/app/src/main/java/com/example/android/sunshine/sync/SunshineSyncUtils.java
+++ S10.02-Exercise-SmarterSyncing/app/src/main/java/com/example/android/sunshine/sync/SunshineSyncUtils.java
@@ -25,11 +25,13 @@ public class SunshineSyncUtils {
     // COMPLETED (1) Declare a private static boolean field called sInitialized
     private static boolean sInitialized;
 
-    //  TODO (2) Create a synchronized public static void method called initialize
+    // COMPLETED (2) Create a synchronized public static void method called initialize
+    public static synchronized void initialize(Context context) {
     //  TODO (3) Only execute this method body if sInitialized is false
     //  TODO (4) If the method body is executed, set sInitialized to true
     //  TODO (5) Check to see if our weather ContentProvider is empty
         //  TODO (6) If it is empty or we have a null Cursor, sync the weather now!
+    }
 
     /**
      * Helper method to perform a sync immediately using an IntentService for asynchronous
