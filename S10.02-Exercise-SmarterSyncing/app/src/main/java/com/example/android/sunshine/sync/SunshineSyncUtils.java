b4d183f74a8fd4aae15eafd0403c0acda3774ba0
geeksploit <geeksploit@shootan.com>
2018-01-25 16:41:00 +0300

    Declare a private static field to store current initialization status

--- S10.02-Exercise-SmarterSyncing/app/src/main/java/com/example/android/sunshine/sync/SunshineSyncUtils.java
+++ S10.02-Exercise-SmarterSyncing/app/src/main/java/com/example/android/sunshine/sync/SunshineSyncUtils.java
@@ -22,7 +22,8 @@ import android.support.annotation.NonNull;
 
 public class SunshineSyncUtils {
 
-//  TODO (1) Declare a private static boolean field called sInitialized
+    // COMPLETED (1) Declare a private static boolean field called sInitialized
+    private static boolean sInitialized;
 
     //  TODO (2) Create a synchronized public static void method called initialize
     //  TODO (3) Only execute this method body if sInitialized is false
