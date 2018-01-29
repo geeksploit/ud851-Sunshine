3672e709f9873f12498fb7ff94539daa7428a2af
geeksploit <geeksploit@shootan.com>
2018-01-29 20:21:51 +0300

    Add individual TextView for weather details

--- S11.01-Exercise-NewListItemLayout/app/src/main/java/com/example/android/sunshine/ForecastAdapter.java
+++ S11.01-Exercise-NewListItemLayout/app/src/main/java/com/example/android/sunshine/ForecastAdapter.java
@@ -163,8 +163,11 @@ class ForecastAdapter extends RecyclerView.Adapter<ForecastAdapter.ForecastAdapt
      * OnClickListener, since it has access to the adapter and the views.
      */
     class ForecastAdapterViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
-//      TODO (4) Replace the weatherSummary TextView with individual weather detail TextViews
-        final TextView weatherSummary;
+        // COMPLETED (4) Replace the weatherSummary TextView with individual weather detail TextViews
+        final TextView textWeatherDate;
+        final TextView textWeatherCondition;
+        final TextView textWeatherTemperatureMin;
+        final TextView textWeatherTemperatureMax;
 
 //      TODO (5) Add an ImageView for the weather icon
 
